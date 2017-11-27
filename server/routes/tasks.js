91c844599f537dfe420bda99d3b112f70be71473
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-11-27 10:30:21 -0600

    add put and delete routes on server

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -14,7 +14,7 @@ router.get('/', function (req, res) {
         } else {
             // We connected to the database!!!
             // Now, we're going to GET things from the DB
-            client.query('SELECT * FROM tasks ORDER BY id;', function (errorMakingQuery, result) {
+            client.query('SELECT * FROM tasks ORDER BY is_complete, id;', function (errorMakingQuery, result) {
                 done();
                 if (errorMakingQuery) {
                     // Query failed. Did you test it in Postico?
@@ -49,6 +49,59 @@ router.post('/', function (req, res) {
                     res.sendStatus(500);
                 } else {
                     res.sendStatus(201);
+                }
+            });
+        }
+    });
+});
+
+router.put('/complete/:id', function (req, res) {
+    var taskId = req.params.id;
+    // Attempt to connect to database
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            // There was an error connecting to the database
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            // We connected to the database!!!
+            // Now, we're going to GET things from thd DB
+            client.query(`UPDATE tasks SET is_complete=TRUE
+            WHERE id=$1;`, [taskId], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    // Query failed. Did you test it in Postico?
+                    // Log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(200);
+                }
+            });
+        }
+    });
+});
+
+router.delete('/:id', function (req, res) {
+    var taskId = req.params.id;
+    // Attempt to connect to database
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            // There was an error connecting to the database
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            // We connected to the database!!!
+            // Now, we're going to GET things from thd DB
+            client.query(`DELETE FROM tasks WHERE id=$1;`, [taskId], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    // Query failed. Did you test it in Postico?
+                    // Log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(200);
                 }
             });
         }
